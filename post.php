a65fbebd3d0f01d545f5fc1b3e9bfc784e383a4c
Kjetil Rohde Jakobsen <kjetil@nettnaut.no>
2018-06-18 00:32:35 +0800

    Add staging urls

--- post.php
+++ post.php
@@ -1,7 +1,16 @@
 <?php
 $data_string = file_get_contents('data.json');
 
-$ch = curl_init('http://localhost:8080/webhook.php');
+$host = $_SERVER["HTTP_HOST"];
+
+if ($host == "localhost:8080") {
+    $url = "http://localhost:8080/webhook.php";
+}
+else {
+    $url = "http://home.nettnaut.no/webhook.php";
+}
+
+$ch = curl_init($url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_HEADER, 1);
@@ -14,4 +23,12 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 $result = curl_exec($ch);
 echo curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
+if (curl_error($ch)) {
+    $error_msg = curl_error($ch);
+}
+curl_close($ch);
+
+if (isset($error_msg)) {
+    echo $error_msg;
+}
 ?>
