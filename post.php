4eff36b01866f8d0e385a5e64103a3c0a25e4bfc
Kjetil Rohde Jakobsen <kjetil@nettnaut.no>
2018-06-18 00:12:25 +0800

    Creating webhook for subscription events

---
+++ post.php
@@ -0,0 +1,17 @@
+<?php
+$data_string = file_get_contents('data.json');
+
+$ch = curl_init('http://home.nettnaut.no/webhook.php');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($data_string))
+);
+
+$result = curl_exec($ch);
+echo curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+?>
